6d244d7b323d1749176aecd14d253bd77943480c
Bobbe <maxschlecht@web.de>
2023-05-30 23:08:30 +0200

    Rearrange NodeBase methods

--- __init__.py
+++ __init__.py
@@ -11,54 +11,6 @@ class CustomNodetreeNodeBase:
     _node_def_hash = None
     current_hash: bpy.props.StringProperty(options={"HIDDEN"})
 
-    def init(self, context):
-        cls = self.__class__
-
-        self.current_hash = self.get_node_def_hash()
-
-        if not (node_tree := self.node_tree):
-            node_tree_name = f"CUSTOM_NODE_{cls.__name__}"
-            node_tree = bpy.data.node_groups.new(node_tree_name, "ShaderNodeTree")
-
-        node_tree.nodes.clear()
-        CustomNodetreeNodeBase.setup_inputs(node_tree, cls.inputs_def)
-        CustomNodetreeNodeBase.setup_nodes(node_tree, cls.nodes_def)
-        CustomNodetreeNodeBase.setup_outputs(node_tree, cls.outputs_def)
-        self.node_tree = node_tree
-        self.update_custom_node()
-
-    def update_custom_node(self):
-        self.update()
-
-    def _upgrade_custom_node(self):
-        if self.current_hash == self.get_node_def_hash():
-            return
-        print(f"{COLOR_INFO}[custom_node_utils] updating '{self.node_tree.name}'{COLOR_END}")
-        CustomNodetreeNodeBase.init(self, bpy.context)
-
-    def copy(self, node):
-        self.node_tree = node.node_tree.copy()
-        self.current_hash = node.current_hash
-        self.update_custom_node()
-
-    def free(self):
-        if self.node_tree.users < 1:
-            bpy.data.node_groups.remove(self.node_tree)
-
-    def draw_buttons(self, context, layout):
-        for prop in self.bl_rna.properties:
-            if prop.is_runtime and not (prop.is_readonly or prop.is_hidden):
-                text = "" if prop.type == "ENUM" else prop.name
-                layout.prop(self, prop.identifier, text=text)
-
-    @classmethod
-    def get_node_def_hash(cls):
-        if cls._node_def_hash is None:
-            hash_content = (cls.inputs_def, cls.nodes_def, cls.outputs_def)
-            node_def_json = json.dumps(hash_content, default=str)
-            cls._node_def_hash = hashlib.sha1(node_def_json.encode()).hexdigest()
-        return cls._node_def_hash
-
     @staticmethod
     def setup_inputs(node_tree, inputs_def):
         node_tree.inputs.clear()
@@ -137,6 +89,55 @@ class CustomNodetreeNodeBase:
             else:
                 from_node, output_index = value
                 links.new(nodes[from_node].outputs[output_index], node_output.inputs[name])
+
+    def init(self, context):
+        cls = self.__class__
+
+        self.current_hash = self.get_node_def_hash()
+
+        if not (node_tree := self.node_tree):
+            node_tree_name = f"CUSTOM_NODE_{cls.__name__}"
+            node_tree = bpy.data.node_groups.new(node_tree_name, "ShaderNodeTree")
+
+        node_tree.nodes.clear()
+        CustomNodetreeNodeBase.setup_inputs(node_tree, cls.inputs_def)
+        CustomNodetreeNodeBase.setup_nodes(node_tree, cls.nodes_def)
+        CustomNodetreeNodeBase.setup_outputs(node_tree, cls.outputs_def)
+        self.node_tree = node_tree
+        self.update_custom_node()
+
+    def update_custom_node(self):
+        self.update()
+        # self.socket_value_update(bpy.context)
+
+    def _upgrade_custom_node(self):
+        if self.current_hash == self.get_node_def_hash():
+            return
+        print(f"{COLOR_INFO}[custom_node_utils] updating '{self.node_tree.name}'{COLOR_END}")
+        CustomNodetreeNodeBase.init(self, bpy.context)
+
+    def copy(self, node):
+        self.node_tree = node.node_tree.copy()
+        self.current_hash = node.current_hash
+        self.update_custom_node()
+
+    def free(self):
+        if self.node_tree.users < 1:
+            bpy.data.node_groups.remove(self.node_tree)
+
+    def draw_buttons(self, context, layout):
+        for prop in self.bl_rna.properties:
+            if prop.is_runtime and not (prop.is_readonly or prop.is_hidden):
+                text = "" if prop.type == "ENUM" else prop.name
+                layout.prop(self, prop.identifier, text=text)
+
+    @classmethod
+    def get_node_def_hash(cls):
+        if cls._node_def_hash is None:
+            hash_content = (cls.inputs_def, cls.nodes_def, cls.outputs_def)
+            node_def_json = json.dumps(hash_content, default=str)
+            cls._node_def_hash = hashlib.sha1(node_def_json.encode()).hexdigest()
+        return cls._node_def_hash
 
 class SharedCustomNodetreeNodeBase(CustomNodetreeNodeBase):
     def init(self, context):
